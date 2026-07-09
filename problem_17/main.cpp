fa87f846b47d4be7e69a76cba71f0c03c7bdb7c4
assmabaziz <assma.baziz@yahoo.com>
2026-07-09 11:11:28 +0300

    task 17

---
+++ problem_17/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int base;
+    short int height;
+    float area;
+
+
+    cout << "Enter the base: \n" ;
+    cin >> base ;
+    cout << "Enter the height: \n" ;
+    cin >> height ;
+    area = (base * height) / 2.0 ;
+    cout << "The area of triangle equal to: " << area ;
+
+
+
+
+    return 0;
+}
