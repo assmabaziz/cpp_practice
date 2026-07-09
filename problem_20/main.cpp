4ace684edb4198e7784f852c644c1af304d6112d
assmabaziz <assma.baziz@yahoo.com>
2026-07-09 11:45:42 +0300

    task 20

--- problem_20/main.cpp
+++ problem_20/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    short int diameter ;
+    const float Pi = 3.14 ;
+    float area ;
+
+    cout << "Enter the diameter: \n" ;
+    cin >> diameter ;
+    area = (Pi * diameter * diameter ) / 4 ;
+    cout << "The circle area equals to: " << area << endl ;
+
+
     return 0;
 }
