bb745b6487b18bf216689ae0375efa0c8e99ecac
assmabaziz <assma.baziz@yahoo.com>
2026-07-14 15:51:29 +0300

    task 16

---
+++ problem_16/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    unsigned short int width ;
+    unsigned short int diameter ;
+    float area ;
+
+    cout << "Enter the height:  \n" ;
+    cin >> width ;
+    cout << "Enter the diameter: \n" ;
+    cin >> diameter ;
+    area = width * sqrt(pow(diameter,2) - pow(width , 2)) ;
+    cout << area ;
+
+
+
+    return 0;
+}
