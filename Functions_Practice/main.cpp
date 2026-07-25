a7d2b5f93207ad9a213feb38f7bcad3e5837442e
assmabaziz <assma.baziz@yahoo.com>
2026-07-25 12:29:25 +0300

    some practice on functions

---
+++ Functions_Practice/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void funcPrintData ()
+{
+    cout << "***********************************************\n" ;
+    cout << "Name: Assma Baziz \n" ;
+    cout << "Age: 33 \n" ;
+    cout << "City: Saint Petersburg \n" ;
+    cout << "Country: Russia \n" ;
+    cout << "***********************************************\n" ;
+}
+
+void funcPrintStars()
+{
+    cout << "******************* \n" ;
+    cout << "******************* \n" ;
+    cout << "******************* \n" ;
+}
+
+void funcPintWishes ()
+{
+    cout << "I love programming \n " ;
+    cout << "I promise to be the best developer ever" << endl ;
+    cout << "I know it will take some time to practice but I'll achieve my goal inshalah \n" ;
+    cout << "Best regards \n" ;
+    cout << "Assma Baziz \n" ;
+}
+
+void funcPrintH ()
+{
+    cout << "**      **\n" ;
+    cout << "**      **\n" ;
+    cout << "**      **\n" ;
+    cout << "**********\n" ;
+    cout << "**********\n" ;
+    cout << "**      **\n" ;
+    cout << "**      **\n" ;
+    cout << "**      **" << endl ;
+
+}
+
+int main()
+{
+    funcPrintData() ;
+    funcPintWishes() ;
+    funcPrintStars() ;
+    funcPrintH() ;
+    return 0;
+}
