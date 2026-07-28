c5e026aa96036bcaa57708b7440769ab6cc73622
assmabaziz <assma.baziz@yahoo.com>
2026-07-28 20:30:31 +0300

    Parameters practice problem_32

--- Parameters_practice/problem_32/main.cpp
+++ Parameters_practice/problem_32/main.cpp
@@ -3,19 +3,27 @@
 
 using namespace std;
 
+int funcCalcPower (int baseNumber , int  powerNumber)
+{
+    int Result ;
+    Result = pow(baseNumber , powerNumber) ;
+    return Result ;
+}
+
+
 int main()
 {
     short baseNumber ;
     short powerNumber ;
-    unsigned int result ;
+    int Result ;
 
     cout << "Please enter the base:  \n" ;
     cin >> baseNumber ;
     cout << "Please enter the power: \n" ;
     cin >> powerNumber ;
-    result = pow(baseNumber , powerNumber) ;
-    cout << "The number: " << baseNumber << " to power: " << powerNumber << " equals to: " << result << endl ;
-    cout << "The round of area equals to: " << round(result) ;
+    Result = funcCalcPower(baseNumber , powerNumber) ;
+    cout << "The number: " << baseNumber << " to power: " << powerNumber << " equals to: " << Result << endl ;
+    cout << "The round of area equals to: " << round(Result) ;
 
     return 0;
 }
