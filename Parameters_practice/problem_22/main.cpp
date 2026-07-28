d2af809298ee79c1a64767187a76ed8f3485df5a
assmabaziz <assma.baziz@yahoo.com>
2026-07-28 19:39:47 +0300

    Parameters practice problem_22

--- Parameters_practice/problem_22/main.cpp
+++ Parameters_practice/problem_22/main.cpp
@@ -3,21 +3,26 @@
 
 using namespace std;
 
+float funcCalcArea(unsigned short firstDimension , unsigned short secondDimension)
+{
+    const float Pi = 3.14 ;
+    return (Pi * ((firstDimension * secondDimension) / 4 )) * (( 2.0 * firstDimension - secondDimension ) / (2.0 * firstDimension + secondDimension)) ;
+}
+
 int main()
 {
 
-    const float Pi = 3.14 ;
-    short int firstDimension ;
-    short int secondDimension ;
-    float area ;
+    unsigned short firstDimension ;
+    unsigned short secondDimension ;
+    float Area ;
 
     cout << "Enter the first dimension: \n" ;
     cin >> firstDimension ;
     cout << "Enter the second dimension: \n" ;
     cin >> secondDimension ;
-    area = (Pi * ((firstDimension * secondDimension) / 4 )) * (( 2.0 * firstDimension - secondDimension ) / (2.0 * firstDimension + secondDimension)) ;
-    cout << "The circle area equals to: " << area << endl;
-    cout << "The floor of area equals to: " << floor(area) ;
+    Area = funcCalcArea(firstDimension , secondDimension) ;
+    cout << "The circle area equals to: " << Area << endl;
+    cout << "The floor of area equals to: " << floor(Area) ;
 
     return 0;
 }
