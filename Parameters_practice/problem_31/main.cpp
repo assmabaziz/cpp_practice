0edcbde122634c5e09f4568eadeee0858fb7911b
assmabaziz <assma.baziz@yahoo.com>
2026-07-28 20:21:21 +0300

    Parameters practice problem_31

--- Parameters_practice/problem_31/main.cpp
+++ Parameters_practice/problem_31/main.cpp
@@ -3,21 +3,30 @@
 
 using namespace std;
 
-int main()
+void procedureCalcPower(int UserNumber)
 {
+
     short int firstNumber;
     short int secondNumber;
     short int thirdNumber ;
-    short int userNumber;
 
-    cout << "Enter a number: " ;
-    cin >> userNumber ;
-    firstNumber = pow(userNumber , 2) ;
-    secondNumber = pow(userNumber , 3) ;
-    thirdNumber = pow(userNumber , 4) ;
+    firstNumber = pow(UserNumber , 2) ;
+    secondNumber = pow(UserNumber , 3) ;
+    thirdNumber = pow(UserNumber , 4) ;
     cout << "Your number power 2 : " << firstNumber  << endl ;
     cout << "Your number power 3 : " << secondNumber << endl ;
     cout << "Your number power 4 : " << thirdNumber << endl ;
+
+}
+
+int main()
+{
+    int UserNumber;
+
+    cout << "Enter a number: " ;
+    cin >> UserNumber ;
+
+    procedureCalcPower(UserNumber) ;
 
     return 0;
 }
