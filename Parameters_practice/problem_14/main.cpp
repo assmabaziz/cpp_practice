35599c02ca9cfe8bb25f807d12e5363ac5b2c8b4
assmabaziz <assma.baziz@yahoo.com>
2026-07-28 18:52:51 +0300

    Parameters practice problem_14

---
+++ Parameters_practice/problem_14/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void procedurePrintNumbers ()
+{
+
+    short int firstNumber;
+    short int secondNumber;
+
+    cout << "Enter the first number: \n" ;
+    cin >> firstNumber;
+    cout << "Enter the second number: \n" ;
+    cin >> secondNumber;
+    cout << firstNumber << endl;
+    cout << secondNumber << endl;
+    cout << secondNumber << endl;
+    cout << firstNumber << endl;
+
+}
+
+
+
+
+int main()
+{
+    procedurePrintNumbers() ;
+
+    return 0;
+}
