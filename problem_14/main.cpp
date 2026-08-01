6a1997a79581731964190625ff57e4adc4ddb5a4
assmabaziz <assma.baziz@yahoo.com>
2026-08-01 22:28:33 +0300

    params by Ref

--- problem_14/main.cpp
+++ problem_14/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-void procedureSwapNum(int Num1 , int Num2 )
+void procedureSwapNum(int &Num1 , int &Num2 )
 {
     int TempVar ;
     cout << "The first number before swap: " << Num1  << "\n" ;
@@ -28,6 +28,9 @@ int main()
     procedureSwapNum(FirstNumber , SecondNumber);
     cout << "The ref of first number: " << &FirstNumber << "\n" ;
     cout << "The ref of second number: " << &SecondNumber << "\n" ;
+
+    cout << "First Number after swap inside the main function: " << FirstNumber << "\n" ;
+    cout << "Second Number after swap inside the main function: " << SecondNumber << "\n" ;
 
     return 0;
 }
