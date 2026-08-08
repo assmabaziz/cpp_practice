46827469f6a8bda88449ad90df9c3f0891171580
assmabaziz <assma.baziz@yahoo.com>
2026-08-08 23:21:03 +0300

    Problem 49 with functions and procedures

--- Problem_49/Problem_49.cpp
+++ Problem_49/Problem_49.cpp
@@ -2,21 +2,31 @@
 
 using namespace std;
 
+short PinCode;
+short const Balace = 7500;
+
+void readPinCode(short& PinCode)
+{
+    cout << "Enter your pin code: \n";
+    cin >> PinCode;
+}
+
+void checkPinCode(short PinCode)
+{
+    if (PinCode == 1234)
+    {
+        cout << "Your Balnce: " << Balace;
+    }
+    else
+    {
+        cout << "Wrong Pin code";
+    }
+}
+
 int main()
 {
    short  PinCode ;
-   short const Balace = 7500;
-
-   cout << "Enter your pin code: \n";
-   cin >> PinCode;
-
-   if (PinCode == 1234)
-   {
-       cout << "Your balance is:" << Balace;
-   }
-   else
-   {
-       cout << "Wrong Pin code";
-   }
+   readPinCode(PinCode);
+   checkPinCode(PinCode);
     return 0;
 }
