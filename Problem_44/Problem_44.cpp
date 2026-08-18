23c1d269201bde9f62efd89c361e8c6a49f34411
assmabaziz <assma.baziz@yahoo.com>
2026-08-18 10:30:45 +0300

    Resolve problem 44 using enums and nested functions

--- Problem_44/Problem_44.cpp
+++ Problem_44/Problem_44.cpp
@@ -3,57 +3,70 @@
 
 using namespace std;
 
-short readDayNumber()
+enum enWeekDays {Sun = 1, Mon = 2, Tue = 3, Wed = 4, Thur = 5, Fri = 6, Satu = 7};
+
+void readDayNumber()
 {
-    short  DayNumber;
-    cout << "Please enter the day number \n";
-    cin >> DayNumber;
-    return DayNumber;
+    cout << "Please choose the day number \n";
+    cout << "*********************************\n";
+    cout << "1: Sunday\n";
+    cout << "2: Monday\n";
+    cout << "3: Tuesday\n";
+    cout << "4: Wednsday\n";
+    cout << "5: Thursday\n";
+    cout << "6: Friday\n";
+    cout << "7: Saturday\n";
+    cout << "*********************************\n";
 }
-string identifyWeekDay(short  DayNumber)
+
+enWeekDays GetWeekDayNumber()
 {
-    string WeekDay;
-
-    if (DayNumber >= 1 && DayNumber <= 7 )
-    {
-        switch (DayNumber)
-        {
-        case 1:
-            WeekDay = "Saturday";
-            break;
-
-        case 2:
-            WeekDay = "Sunday";
-            break;
-
-        case 3:
-            WeekDay = "Monday";
-            break;
-
-        case 4:
-            WeekDay = "Tuesday";
-            break;
-
-        case 5:
-            WeekDay = "Wednesday";
-            break;
-
-        case 6:
-            WeekDay = "Thursday";
-            break;
-
-        case 7:
-            WeekDay = "Friday";
-            break;
-        }
-    }
-    else
-    {
-        cout << "Please enter a valid number";
-    }
-    return WeekDay;
+    short  UserNumber;
+    cin >> UserNumber;
+    return (enWeekDays)UserNumber;
 }
+
+string GetWeekDayName(enWeekDays  DayNumber)
+{
+
+    switch (DayNumber)
+    {
+    case enWeekDays::Sun:
+        return  "Sunday";
+        break;
+
+    case enWeekDays::Mon:
+        return "Monday";
+        break;
+
+    case enWeekDays::Tue:
+        return "Tuesday";
+        break;
+
+    case enWeekDays::Wed:
+        return "Wednesday";
+        break;
+
+    case enWeekDays::Thur:
+        return "Thursday";
+        break;
+
+    case enWeekDays::Fri:
+        return "Friday";
+        break;
+
+    case enWeekDays::Satu:
+        return "Saturday";
+        break;
+    default:
+        return "Please enter a valid number from 1 to 7";
+       
+    }  
+}
+
 int main()
 {
-   cout <<  identifyWeekDay(readDayNumber());
+   readDayNumber();
+   cout << GetWeekDayName(GetWeekDayNumber());
+   
 }
